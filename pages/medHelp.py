3dfa00eabdbeaede8a5dfa397c558a8380b06343
Abhiram <113409577+ABHCI15@users.noreply.github.com>
2024-06-07 21:19:30 +0000

    working_version

--- pages/medHelp.py
+++ pages/medHelp.py
@@ -25,10 +25,13 @@ from langchain_google_genai import (
     HarmBlockThreshold,
     HarmCategory,
 )
+from langchain_community.utilities import ArxivAPIWrapper
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.prompts import PromptTemplate
-from langchain_community.tools import WikipediaQueryRun
+from langchain_community.tools import WikipediaQueryRun, ArxivQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+
 
 tool_wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 tool_wikidata = WikidataQueryRun(api_wrapper=WikidataAPIWrapper())
@@ -36,37 +39,47 @@ os.environ["TAVILY_API_KEY"] = st.secrets["TAVILY_API_KEY"]
 # TavilySearchAPIWrapper = TavilySearchAPIWrapper(tavily_api_key=st.secrets["TAVILY_API_KEY"])
 tool_med = PubmedQueryRun()
 tool_search = TavilySearchResults()
-
+tool_arxiv = ArxivQueryRun(api_wrapper=ArxivAPIWrapper())
 
 
 st.title("Medical Help Chatbot")
 
-with st.chat_message("assistant"):
-    st.write("Hello 👋, Welcome to Med Q&A, please note that this is not intended to provide medical advice and merely exists as a small research aid.")
 
 text = st.chat_input(placeholder="Type your message here...")
-template = template = """
-Answer the following questions as best you can. You are a very helpful medically oriented assistant.
-If the action does not yield the desired result, you can always try another action or the same action with different input. But always give a final answer. Feel free to cross reference with other tools after you check pubmed if needed, but minimize use of tavily as it costs financial resources.
+# template = template = """
+# Answer the following questions as best you can. You are a very helpful medically oriented assistant.
+# If the action does not yield the desired result, you can always try another action or the same action with different input. But always give a final answer. Feel free to cross reference with other tools after you check pubmed if needed, but minimize use of tavily as it costs financial resources.
 
-Use the following format:
+# Use the following format:
 
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of the tools you have access to
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat 5 times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+# Question: the input question you must answer
+# Thought: you should always think about what to do
+# Action: the action to take, should be one of the tools you have access to
+# Action Input: the input to the action
+# Observation: the result of the action
+# ... (this Thought/Action/Action Input/Observation can repeat 5 times)
+# Thought: I now know the final answer
+# Final Answer: the final answer to the original input question
 
-Begin!
+# Begin!
 
-Question: {input}
-Thought:{agent_scratchpad}
-"""
-prompt = PromptTemplate.from_template(template)
-tools = [tool_med, tool_search, tool_wikipedia, tool_wikidata]
+# Question: {input}
+# Thought:{agent_scratchpad}
+# """
+# prompt = PromptTemplate.from_template(template)
+
+prompt = prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful medical assistant, who will try your best to deliver accurate and helpful information to the user. You have access to the following tools: Pubmed, Tavily Search, Wikipedia, and Wikidata. You can use these tools to help answer the user's questions. Prefer pubmed over tavily search, and minimize tavily search usage as it costs financial resources. You can cross reference with other tools after you check pubmed if needed. Make sure to cite your sources and provide an in depth analysis of the user's question. You will not get stuck when answering a question, which you tended to do before you recieved your MD. You will always give a final answer. If the action does not yield the desired result, you can always try another action or the same action with different input. For Example Arxiv and Pubmed are both excellent sources for scientific papers.",
+        ),
+        ("placeholder", "{history}"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
+tools = [tool_med, tool_search, tool_wikipedia, tool_wikidata, tool_arxiv]
 # prompt.format(tools=tools, tool_names=[tool.name for tool in tools])
 llm = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash", temperature=0.5, google_api_key=st.secrets["api_key"], safety_settings={ HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE})
 # llm.bind_tools(tools)
@@ -75,11 +88,18 @@ agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 
 history = StreamlitChatMessageHistory(key="chat_messages") # add user id as key
+if len(history.messages) == 0:
+    history.add_ai_message("Hello 👋, Welcome to Med Q&A, please note that this is not intended to provide medical advice and merely exists as a small research aid.")
 
+for msg in history.messages:
+    st.chat_message(msg.type).write(msg.content)
+    
 if prompt :=text:
     with st.chat_message("user"):
         st.write(prompt)
+        history.add_user_message(prompt)
     with st.chat_message("assistant"):
-        out = agent_executor.invoke({"input": prompt})
-        print(out)
-        st.markdown(f"```{out}```")
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent_executor.invoke({"input": prompt, "history": history.messages}, {"callbacks": [st_callback]})
+        st.write(response["output"])
+        history.add_ai_message(response["output"])
